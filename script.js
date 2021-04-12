4e0f2a230ae09803ff97b705a17b4a0b1cac1961
Valerie Michalecki <valerie227@gmail.com>
2021-04-12 18:47:38 -0500

    created a function that randomizes the arrays

--- script.js
+++ script.js
@@ -1,103 +1,128 @@
 // Assignment Code
 let generateBtn = document.querySelector('#generate');
-let passwordParams = {
-	specialCharacters: [
-		'@',
-		'%',
-		'+',
-		'\\',
-		'/',
-		"'",
-		'!',
-		'#',
-		'$',
-		'^',
-		'?',
-		':',
-		',',
-		')',
-		'(',
-		'}',
-		'{',
-		']',
-		'[',
-		'~',
-		'-',
-		'_',
-		'.',
-	],
-	numericCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-	lowerCasedCharacters: [
-		'a',
-		'b',
-		'c',
-		'd',
-		'e',
-		'f',
-		'g',
-		'h',
-		'i',
-		'j',
-		'k',
-		'l',
-		'm',
-		'n',
-		'o',
-		'p',
-		'q',
-		'r',
-		's',
-		't',
-		'u',
-		'v',
-		'w',
-		'x',
-		'y',
-		'z',
-	],
-	upperCasedCharacters: [
-		'A',
-		'B',
-		'C',
-		'D',
-		'E',
-		'F',
-		'G',
-		'H',
-		'I',
-		'J',
-		'K',
-		'L',
-		'M',
-		'N',
-		'O',
-		'P',
-		'Q',
-		'R',
-		'S',
-		'T',
-		'U',
-		'V',
-		'W',
-		'X',
-		'Y',
-		'Z',
-	],
-};
-let passwordLength = window.prompt('How many characters? (Enter 8-128)');
-let lowerCase = window.confirm('Include lowercased characters?');
-let upperCase = window.confirm('Include uppercased characters?');
-let numeric = window.confirm('Include numeric characters?');
-let special = window.confirm('Include special characters?');
+// let passwordParams = {
+const specialCharacters = [
+	'@',
+	'%',
+	'+',
+	'\\',
+	'/',
+	"'",
+	'!',
+	'#',
+	'$',
+	'^',
+	'?',
+	':',
+	',',
+	')',
+	'(',
+	'}',
+	'{',
+	']',
+	'[',
+	'~',
+	'-',
+	'_',
+	'.',
+];
+const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const lowerCasedCharacters = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+];
+const upperCasedCharacters = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z',
+];
 
-// Write password to the #password input
+function passwordParams() {
+	// variable to store password length
+	const passwordLength = parseInt(window.prompt('How many characters? (Enter 8-128)'));
+	const lowerCase = window.confirm('Include lowercased characters?');
+	const upperCase = window.confirm('Include uppercased characters?');
+	const numeric = window.confirm('Include numeric characters?');
+	const special = window.confirm('Include special characters?');
+
+	// if statement to validate
+	if (lowerCase != true && upperCase != true && numeric != true && special != true) {
+		alert('You must choose at least one parameter.');
+		return;
+	}
+	const pwParams = {
+		length: passwordLength,
+		lowerChar: lowerCase,
+		upperChar: upperCase,
+		specialChar: special,
+		number: numeric,
+	};
+	return pwParams;
+}
+
+// Write password to the #password inputmmi
+
+//create a function that randomizes everything (4 things at the same time)
+
+function randomizer(array) {
+	const index = Math.floor(Math.random() * array.length);
+	const indexEl = array[index];
+	return indexEl;
+}
+
+console.log(randomizer(numericCharacters));
 
 function writePassword() {
-	let password = generatePassword();
-	let passwordText = document.querySelector('#password');
-	for (var i = 0; i < passwordLength.length; i++) {
-		let index = Math.floor(Math.random() * lowerCase.length);
-	}
+	const password = generatePassword();
+	const passwordText = document.querySelector('#password');
+	for (var i = 0; i < passwordLength.length; i++) {}
 	passwordText.value = password;
 }
 
