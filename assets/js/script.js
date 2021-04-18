9c5711afff80684be3d2319022429c87b03d85fe
Valerie Michalecki <valerie227@gmail.com>
2021-04-17 19:18:24 -0500

    fixed loop so length requirements are checked, organized folders, added a readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,9 +86,18 @@ const upperCasedCharacters = [
 // this function prompts the user to make a choice with popup windows and then evaluates the user's choices in a string for the length and booleans for the rest of the parameters
 function passwordParams() {
 	// Variables to store password parameters
-	const passwordLength = parseInt(window.prompt('How many characters? (Enter 8-128)'));
-	const lowerCase = window.confirm('Include lowercased characters?');
-	const upperCase = window.confirm('Include uppercased characters?');
+
+	let passwordLength = parseInt(window.prompt('How many characters? (Enter 8-128)'));
+	if (passwordLength < 8) {
+		alert('you must choose between 8-128');
+		return;
+	}
+	if (passwordLength > 128) {
+		alert('you must choose between 8-128');
+		return;
+	}
+	const lowerCase = window.confirm('Include lowercase characters?');
+	const upperCase = window.confirm('Include uppercase characters?');
 	const numeric = window.confirm('Include numeric characters?');
 	const special = window.confirm('Include special characters?');
 
